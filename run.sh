2dea9dec417c8078d4f743af12a87015146f8c81
duclong1009 <duclong10092000@gmail.com>
2024-01-31 19:33:51 +0900

    add config

--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
-CUDA_VISIBLE_DEVICES=1 python main.py
+## 
+python main.py --config_path config/ml-1m/PoisonRec/lightgcn.yaml
+python main.py --config_path config/ml-1m/PoisonRec/ngcf.yaml
+
+## 
+python main.py --config_path config/ml-1m/RandomAttack/lightgcn.yaml
+python main.py --config_path config/ml-1m/RandomAttack/ngcf.yaml
+
+
+
+# -------------- Beauty -----------------
+## 
+python main.py --config_path config/Beauty/PoisonRec/lightgcn.yaml
+python main.py --config_path config/Beauty/PoisonRec/ngcf.yaml
+
+## 
+python main.py --config_path config/Beauty/RandomAttack/lightgcn.yaml
+python main.py --config_path config/Beauty/RandomAttack/ngcf.yaml
