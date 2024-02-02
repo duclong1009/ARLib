afcd1c740b9947c3f794968ea5895ee5b0fe6771
duclong1009 <duclong10092000@gmail.com>
2024-02-03 01:58:21 +0900

    change script

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 ## 
 python main.py --config_path config/ml-1m/PoisonRec/lightgcn.yaml
+rm -r 'modelsaved'
 python main.py --config_path config/ml-1m/PoisonRec/ngcf.yaml
-
+rm -r 'modelsaved'
 ## 
 python main.py --config_path config/ml-1m/RandomAttack/lightgcn.yaml
+rm -r 'modelsaved'
 python main.py --config_path config/ml-1m/RandomAttack/ngcf.yaml
-
+rm -r 'modelsaved'
 
 
 # -------------- Beauty -----------------
 ## 
 python main.py --config_path config/Beauty/PoisonRec/lightgcn.yaml
+rm -r 'modelsaved'
 python main.py --config_path config/Beauty/PoisonRec/ngcf.yaml
+rm -r 'modelsaved'
 
 ## 
 python main.py --config_path config/Beauty/RandomAttack/lightgcn.yaml
+rm -r 'modelsaved'
 python main.py --config_path config/Beauty/RandomAttack/ngcf.yaml
+rm -r 'modelsaved'
